3016337d622c354a6ca654ae410f9b4575d51991
Allan Girao <allan.girao@ae.studio>
2025-12-10 12:47:24 -0300

    Fix migration order.

--- server/database_migrations/versions/0016_add_llm_token_usages_table.py
+++ server/database_migrations/versions/0016_add_llm_token_usages_table.py
@@ -10,8 +10,8 @@ import sqlalchemy as sa
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision = "0015"
-down_revision = "0014"
+revision = "0016"
+down_revision = "0015"
 branch_labels = None
 depends_on = None
 
